45a7bca0ca606e17dc45ba6d4bccb4a7109dccb1
RyoNakano <naca.11868@gmail.com>
2021-01-02 20:02:42 +0900

    ニュース検索フォーム追加 ;

--- News/ViewController.swift
+++ News/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //SearchBarのdelegate通知先を設定
+        searchBar.delegate = self
+        //入力のヒントになるう、プレースホルダーを設定
+        searchBar.placeholder = "知りたいニュースを検索"
     }
 
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    //検索ボタンをクリックした時
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //キーボードを閉じる
+        view.endEditing(true)
+        
+        if let searchWord = searchBar.text {
+            print(searchWord)
+        }
+    }
 }
 
